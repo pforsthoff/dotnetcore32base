ce88b95c9d1b96820dee3c71f71670ba16b66c5e
pete.guerette <pete.guerette@gmail.com>
2021-01-18 08:59:03 -0700

    more project rearrangement

---
+++ test/Cheetas3.EU.Infrastructure.UnitTests/Files/Commands/ProvisionFile/CalculateTimeSpanUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Cheetas3.EU.Application.Files.Commands.ProvisionFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheetas3.EU.Application.Files.Commands.DeleteFile.Tests
+{
+    [TestFixture()]
+    public class CalculateTimeSpanUnitTests
+    {
+        [Test()]
+        public void GetTimeSpanTest()
+        {
+            var span = GetTimeSpan(DateTime.Now, DateTime.Now.AddMinutes(120));
+        }
+    }
+}
