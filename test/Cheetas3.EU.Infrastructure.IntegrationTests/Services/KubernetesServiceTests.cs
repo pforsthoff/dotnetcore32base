4a201b3a2a9d2c5905d0a24067170dcee60c578a
Administrator <admin@example.com>
2021-01-19 13:53:28 -0700

    added Cheetas.EU.Infrastructure.IntegrationTests Project

---
+++ test/Cheetas3.EU.Infrastructure.IntegrationTests/Services/KubernetesServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using k8s;
+using k8s.Models;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cheetas3.EU.Infrastructure.IntegrationTests.Services
+{
+    class KubernetesServiceTests
+    {
+        [TestCase("apps-admin@apps")]
+        public void CheckConfigCurrentContext(string context)
+        {
+            var fi = new FileInfo("artifacts/config");
+            var cfg = KubernetesClientConfiguration.BuildConfigFromConfigFile(fi, context, useRelativePaths: false);
+            Assert.AreEqual(context, cfg.CurrentContext);
+        }
+        [TestCase("dotnet-test")]
+        public void CreateReadNamespace(string @namespace)
+        {
+            // Load from the default kubeconfig on the machine.
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("artifacts/config");
+
+            // Use the config object to create a client.
+            var client = new Kubernetes(config);
+
+            var ns = new V1Namespace
+            {
+                Metadata = new V1ObjectMeta
+                {
+                    Name = @namespace
+                }
+            };
+            try
+            {
+                //delete namespace if exists
+                var status = client.DeleteNamespace(ns.Metadata.Name, new V1DeleteOptions());
+            }
+            catch (System.Exception)
+            {
+            }
+            ns = new V1Namespace
+            {
+                Metadata = new V1ObjectMeta
+                {
+                    Name = @namespace
+                }
+            };
+            var result = client.CreateNamespace(ns);
+            var namespaces = client.ListNamespace();
+            foreach (var name in namespaces.Items)
+            {
+                if(name.Metadata.Name == @namespace)
+                {
+                    Assert.AreEqual(name.Metadata.Name, @namespace);
+                }
+            }
+        }
+    }
+}
