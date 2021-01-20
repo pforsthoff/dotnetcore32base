5973048c5307597ae14150edd3e620fd4199477a
pete.guerette <pete.guerette@gmail.com>
2021-01-19 17:37:38 -0700

    Updated K8S Integration Tests

--- test/Cheetas3.EU.Infrastructure.IntegrationTests/Services/KubernetesServiceTests.cs
+++ test/Cheetas3.EU.Infrastructure.IntegrationTests/Services/KubernetesServiceTests.cs
@@ -1,61 +1,50 @@
 ﻿using k8s;
 using k8s.Models;
 using NUnit.Framework;
+using System;
 using System.IO;
 using System.Linq;
+using FluentAssertions;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Cheetas3.EU.Infrastructure.IntegrationTests.Services
 {
     class KubernetesServiceTests
     {
-        [TestCase("apps-admin@apps")]
+
+        private Kubernetes _client;
+        private string _guidNS;
+        private V1Namespace _namespace;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("assets/config");
+            _client = new Kubernetes(config);
+            _guidNS = Guid.NewGuid().ToString();
+            _namespace = new V1Namespace { Metadata = new V1ObjectMeta { Name = _guidNS } };
+        }
+
+        [TestCase("dev-admin@dev"), Order(1)]
         public void CheckConfigCurrentContext(string context)
         {
             var fi = new FileInfo("assets/config");
             var cfg = KubernetesClientConfiguration.BuildConfigFromConfigFile(fi, context, useRelativePaths: false);
             Assert.AreEqual(context, cfg.CurrentContext);
         }
-        [TestCase("nunit-test")]
-        public void CreateReadNamespace(string @namespace)
+        [Test, Order(2)]
+        public async Task CanCreateNamespaceAsync()
         {
-            // Load from the default kubeconfig on the machine.
-            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("assets/config");
+            var result = await _client.CreateNamespaceAsync(_namespace);
+            result.Metadata.Name.Should().BeEquivalentTo(_guidNS);
+        }
 
-            // Use the config object to create a client.
-            var client = new Kubernetes(config);
-
-            var ns = new V1Namespace
-            {
-                Metadata = new V1ObjectMeta
-                {
-                    Name = @namespace
-                }
-            };
-            try
-            {
-                //delete namespace if exists
-                var status = client.DeleteNamespace(ns.Metadata.Name, new V1DeleteOptions());
-            }
-            catch (System.Exception)
-            {
-            }
-            ns = new V1Namespace
-            {
-                Metadata = new V1ObjectMeta
-                {
-                    Name = @namespace
-                }
-            };
-            var result = client.CreateNamespace(ns);
-            var namespaces = client.ListNamespace();
-            foreach (var name in namespaces.Items)
-            {
-                if(name.Metadata.Name == @namespace)
-                {
-                    Assert.AreEqual(name.Metadata.Name, @namespace);
-                }
-            }
+        [Test, Order(3)]
+        public async Task CanDeleteNamespaceAsync()
+        {
+            var result = await _client.DeleteNamespaceAsync(_namespace.Metadata.Name, new V1DeleteOptions());
+            result.HasObject.Should().BeTrue();
         }
     }
 }
